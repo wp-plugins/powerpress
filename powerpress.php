188310b84f442dba1e6c28a9b3e5889df231b6c4
amandato <amandato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-11 19:55:23 +0000

    Incremented version number, releasing 0.7.1 to the public.
    git-svn-id: https://plugins.svn.wordpress.org/powerpress/trunk@101422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powerpress.php
+++ powerpress.php
@@ -3,7 +3,7 @@
 Plugin Name: Blubrry PowerPress
 Plugin URI: http://www.blubrry.com/powerpress/
 Description: <a href="http://www.blubrry.com/powerpress/" target="_blank">Blubrry PowerPress</a> adds podcasting support to your blog. Features include: media player, 3rd party statistics and iTunes integration.
-Version: 0.6.5
+Version: 0.7.1
 Author: Blubrry
 Author URI: http://www.blubrry.com/
 Change Log:
